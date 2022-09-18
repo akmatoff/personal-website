9fd6b9b78b8338639392e55787c0b2dd8190f07a
akmatoff <akmatoff.exe@gmail.com>
2022-09-18 22:29:37 +0600

    Working on hero section

--- components/pages/Home/HeroSection.tsx
+++ components/pages/Home/HeroSection.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import Section from "../../shared/Section/Section";
+
+import styles from "./HeroSection.module.css";
 
 function HeroSection() {
   return (
     <Section id="home">
-      HOME
+      <div className="flex row centered">
+
+        <div className="flex column">
+          <div className={styles['hero-heading-text']}>Welcome to my blog</div>
+
+          <div className={styles['hero-body-text']}>
+            My personal blog where I write about various topics like programming, music and other things.
+          </div>
+        </div>
+
+      </div>
     </Section>
-  )
+  );
 }
 
 export default HeroSection;
